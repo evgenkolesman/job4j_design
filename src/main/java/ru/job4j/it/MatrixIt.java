ca5bd84d95e23169d644f31efefe137abd8bbafd
glavs <glavstroi_e@mail.ru>
2021-01-25 10:33:06 +0300

    Init travis & jacoco

--- src/main/java/ru/job4j/it/MatrixIt.java
+++ src/main/java/ru/job4j/it/MatrixIt.java
@@ -11,7 +11,28 @@ public class MatrixIt implements Iterator<Integer> {
         this.data = data;
     }
     public boolean hasNext(){
-     if (row < data.length&& column < data[row].length) return true;
+     if (row < data.length&& column < data[row].length){
+         while (row <= data.length && column>=data[row].length) // это для исключения пустых частей
+         row=0;
+         column++;
+     return (row < data.length && column < data[row].length);}
+     else
+         return false;
+    }
+
+     public Integer next() // включаем перербор
+        {
+            if(!hasNext())
+            {
+                throw new NoSuchElementException(); // это для исключений
+            }
+            int data1 = data[row][column];
+            column++;
+     return data1;
+        }
+}
+         /*return true;
+         }
     else
         return false; // это по условиям задачи, но есть вариант сократить просто
      //return (row < data.length && column < data[row].length); - думаю вот так
@@ -24,11 +45,9 @@ public class MatrixIt implements Iterator<Integer> {
         }
         int data1 = data[row][column];
         column++;
-        while (row <= data.length && column>=data[row].length) // это для исключения пустых частей
+        /*while (row <= data.length && column>=data[row].length) // это для исключения пустых частей
         {
             row=0;
             column++;
-        }
-        return data1;
-    }
-}
+        }*/
+      // было return data1ж
