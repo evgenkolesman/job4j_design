a4fa7d996b0dc55241a29f8dc5610b91c90969f2
glavs <glavstroi_e@mail.ru>
2021-01-25 09:55:59 +0300

    Init travis & jacoco

--- src/main/java/ru/job4j/it/BackwardArrayIt.java
+++ src/main/java/ru/job4j/it/BackwardArrayIt.java
@@ -4,15 +4,17 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 public class BackwardArrayIt implements Iterator<Integer> {
-    private final int data [];
-    private int point=0;
+    private final int[] data;
+    private int point = 0;
 
-    public BackwardArrayIt(int data[]){
-        this.data=data;
+    public BackwardArrayIt(int[] data) {
+    this.data = data;
     }
-    public boolean hasNext(){
-        return point<data.length;
+
+    public boolean hasNext() {
+        return point < data.length;
     }
+
     public Integer next() {
         if (!hasNext()) {
             throw new NoSuchElementException();
