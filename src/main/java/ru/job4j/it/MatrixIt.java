f4475c4af1c102dc96dfd23393c740f9dafe88f1
glavs <glavstroi_e@mail.ru>
2021-01-26 09:08:39 +0300

    Init travis & jacoco

--- src/main/java/ru/job4j/it/MatrixIt.java
+++ src/main/java/ru/job4j/it/MatrixIt.java
@@ -5,12 +5,14 @@ import java.util.NoSuchElementException;
 
 public class MatrixIt implements Iterator<Integer> {
     private final int[][]data; // убрал final так как выдает в дальнейшем ошибку, ошибка была из-за того что не объявил в методе MatrixIt
-    private int row=0;
-    private int column=0;
+    private int row = 0;
+    private int column = 0;
+
     public MatrixIt(int[][] data) {
         this.data = data;
     }
-    public boolean hasNext(){
+
+    public boolean hasNext() {
      //if (row < data.length&& column < data[row].length)
          return (row < data.length && column < data[row].length);
          //return true;
@@ -18,17 +20,15 @@ public class MatrixIt implements Iterator<Integer> {
      //else return false;
     }
 
-     public Integer next() // включаем перербор
-        {
-            if(!hasNext())
-            {
+     public Integer next() {           // включаем перербор
+            if (!hasNext()) {
                 throw new NoSuchElementException(); // это для исключений
             }
             int data1 = data[row][column];
             column++;
-            while (row <= data.length && column>=data[row].length){ // это для исключения пустых частей
-                row=0;
-            column++;}
+            while (row <= data.length && column >= data[row].length) { // это для исключения пустых частей
+                row = 0;
+            column++; }
      return data1;
         }
 }
