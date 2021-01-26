3650f37be209d2c9e5954de6d16d33292dff9145
glavs <glavstroi_e@mail.ru>
2021-01-26 15:01:05 +0300

    Init travis & jacoco

--- src/main/java/ru/job4j/it/MatrixIt.java
+++ src/main/java/ru/job4j/it/MatrixIt.java
@@ -4,51 +4,74 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 public class MatrixIt implements Iterator<Integer> {
-    private final int[][]data; // убрал final так как выдает в дальнейшем ошибку, ошибка была из-за того что не объявил в методе MatrixIt
+    private final int[][] data; // убрал final так как выдает в дальнейшем ошибку
     private int row = 0;
     private int column = 0;
 
     public MatrixIt(int[][] data) {
         this.data = data;
     }
-
     public boolean hasNext() {
-     //if (row < data.length&& column < data[row].length)
-         return (row < data.length && column < data[row].length);
-         //return true;
-     //(row < data.length && column < data[row].length);}
-     //else return false;
+        return (row < data.length && column < data[row].length);
     }
 
-     public Integer next() {           // включаем перербор
-            if (!hasNext()) {
-                throw new NoSuchElementException(); // это для исключений
-            }
-            int data1 = data[row][column];
-            column++;
-            while (row <= data.length && column >= data[row].length) { // это для исключения пустых частей
-                row = 0;
-            column++; }
-     return data1;
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
         }
-}
-         /*return true;
-         }
-    else
-        return false; // это по условиям задачи, но есть вариант сократить просто
-     //return (row < data.length && column < data[row].length); - думаю вот так
+        int data1 = data[row][column];
+        column++;
+        while (row < data.length && column >= data[row].length) {
+            row = 0;
+            column++;
+        }
+        return data1;
     }
+
+
+}
+/*public class MatrixIt implements Iterator<Integer>
+{
+    private final int  [][] data; // убрал final так как выдает в дальнейшем ошибку
+    private int row=0;
+    private int column=0;
+    private int size;
+    private int poz = 0;
+
+    public MatrixIt (int [][] data) {
+        this.data = data;
+        this.size=countEl(data);
+    }
+
+    public int countEl(int [][] data) {
+        int count = 0;
+        for (int [] row :data) {
+        count+= row.length;
+        }
+        return count;
+        }
+
+
+    public boolean hasNext () {
+    return poz<size;
+    }
+
     public Integer next() // включаем перербор
     {
-        if(!hasNext())
+        if(poz>=size) //ограничивает движение за размер
         {
             throw new NoSuchElementException(); // это для исключений
         }
         int data1 = data[row][column];
         column++;
-        /*while (row <= data.length && column>=data[row].length) // это для исключения пустых частей
+        while (row <= data.length && column >= data[row].length) // это для исключения пустых частей
         {
             row=0;
             column++;
-        }*/
-      // было return data1;
+
+        }
+        return data1;
+    }
+}*/
+
+
--- src/test/java/ru/job4j/it/MatrixItTest.java
+++ src/test/java/ru/job4j/it/MatrixItTest.java
@@ -1,6 +1,5 @@
 package ru.job4j.it;
 
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.Matchers.is;
 
@@ -11,109 +10,85 @@ import java.util.NoSuchElementException;
 public class MatrixItTest {
     @Test
     public void when4El() {
-        MatrixIt it = new MatrixIt(new int[][] {{1}});
-        it.next();
-        it.next();
-        assertFalse(it.hasNext());
-        //assertThat(it.next(), is(1));
+        int[][] in = {
+                {1}
+        };
+        MatrixIt it = new MatrixIt(in);
+        assertThat(it.next(), is(1));
     }
 
     @Test
     public void whenFirstEmptyThenNext() {
-        MatrixIt it = new MatrixIt(new int[][] {{}, {1}});
-        it.next();
-        it.next();
-        assertFalse(it.hasNext());
-        /*int[][] in = {
+        int[][] in = {
                 {}, {1}
         };
         MatrixIt it = new MatrixIt(in);
-        //assertThat(it.next(), is(1));*/
+        assertThat(it.next(), is(1));
+    }
+
+    @Test
+    public void whenFirstEmptyThenHashNext() {
+        int[][] in = {
+                {}, {1}
+        };
+        MatrixIt it = new MatrixIt(in);
+        assertThat(it.hasNext(), is(true));
     }
 
     @Test
     public void whenRowHasDiffSize() {
-        MatrixIt it = new MatrixIt(new int[][] {
-                {1}, {2, 3}});
-        it.next();
-        it.next();
-        assertFalse(it.hasNext());
-
-        /*int[][] in = {
+        int[][] in = {
                 {1}, {2, 3}
         };
         MatrixIt it = new MatrixIt(in);
         assertThat(it.next(), is(1));
         assertThat(it.next(), is(2));
-        assertThat(it.next(), is(3));*/
+        assertThat(it.next(), is(3));
     }
 
     @Test
     public void whenFewEmpty() {
-        MatrixIt it = new MatrixIt(new int[][] {
-                {1}, {}, {}, {}, {2}});
-        it.next();
-        it.next();
-        assertFalse(it.hasNext());
-        /*int[][] in = {
+        int[][] in = {
                 {1}, {}, {}, {}, {2}
         };
         MatrixIt it = new MatrixIt(in);
         assertThat(it.next(), is(1));
-        assertThat(it.next(), is(2));*/
+        assertThat(it.next(), is(2));
     }
 
     @Test
     public void whenEmpty() {
-        MatrixIt it = new MatrixIt(new int[][] {{}});
-        it.next();
-        it.next();
-        assertFalse(it.hasNext());
-
-        /*int[][] in = {
-                {}
-        };
-        MatrixIt it = new MatrixIt(in);
-        assertThat(it.hasNext(), is(false));*/
-    }
-
-    @Test(expected = NoSuchElementException.class)
-    public void whenEmptyThenNext() {
-        MatrixIt it = new MatrixIt(new int[][]{ {} });
-        it.next();
-        it.next();
-        assertFalse(it.hasNext());
-    }
-    /*public void whenEmptyThenNext() {
         int[][] in = {
                 {}
         };
         MatrixIt it = new MatrixIt(in);
-        it.next();*/
+        assertThat(it.hasNext(), is(false));
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenEmptyThenNext() {
+        int[][] in = {
+                {}
+        };
+        MatrixIt it = new MatrixIt(in);
+        it.next();
+    }
 
     @Test
     public void whenMultiHashNext() {
-        MatrixIt it = new MatrixIt(new int[][]{{}, {1}});
-        it.next();
-        it.next();
-        assertFalse(it.hasNext());
-
-        /*int[][] in = {
+        int[][] in = {
                 {}, {1}
         };
         MatrixIt it = new MatrixIt(in);
         assertThat(it.hasNext(), is(true));
-        assertThat(it.hasNext(), is(true));*/
+        assertThat(it.hasNext(), is(true));
     }
 
     @Test
     public void whenNoElements() {
-        MatrixIt it = new MatrixIt(new int[][] {{}, {}, {}});
-        it.next();
-        it.next();
-        assertFalse(it.hasNext());
-        /*int[][] in = {{}, {}, {}};
+        int[][] in = {{}, {}, {}};
         MatrixIt it = new MatrixIt(in);
-        assertThat(it.hasNext(), is(false));*/
+        assertThat(it.hasNext(), is(false));
     }
 }
+
