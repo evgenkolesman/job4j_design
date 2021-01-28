73078a69d36bb8963e33bd789c3759c97a2b33b7
glavs <glavstroi_e@mail.ru>
2021-01-28 14:28:55 +0300

    Init travis & jacoco

--- src/main/java/ru/job4j/it/MatrixIt.java
+++ src/main/java/ru/job4j/it/MatrixIt.java
@@ -39,9 +39,6 @@ public class MatrixIt implements Iterator<Integer> {
         if (!hasNext()) {
             throw new NoSuchElementException();
         }
-        //int data1 = data[row][column];
-        //column++;
-        //return data1;
         return data[row][column++];
     }
 }
