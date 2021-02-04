499a2b315692ce5a0e0ebe7e38e01e5b95b170f6
glavs <glavstroi_e@mail.ru>
2021-02-04 14:10:15 +0300

    Init travis & jacoco

---
+++ src/main/java/ru/job4j/generics/SimpleArray.java
@@ -0,0 +1,30 @@
+/*package ru.job4j.generics;
+
+import java.util.Iterator;
+
+// Каркас.
+public class SimpleArray<T> {
+
+    private T model;
+    int index;
+
+    public static void main (int index, T model) {
+        System.out.println();
+    }
+
+    public add(T model) {
+    }
+
+    public set(int index, T model) {
+    }
+
+    public remove(int index) {
+    }
+
+    public get(int index) {
+    }
+
+    Iterable<T> (Iterator<T> it) {
+        return it = model.iterator(); it.hasNext();
+    }
+}*/
