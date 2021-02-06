c1937cf2a03738bcb12654d3432023e618a03f57
glavs <glavstroi_e@mail.ru>
2021-02-06 13:26:14 +0300

    Init travis & jacoco

--- src/main/java/ru/job4j/generics/SimpleArray.java
+++ src/main/java/ru/job4j/generics/SimpleArray.java
@@ -4,25 +4,30 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 // Каркас.
-public class SimpleArray<T> {
+public class SimpleArray<T> implements Iterable<T> {
 
     private T[] array;
     private T model;
-    private int index;
-
-    public SimpleArray(int index) {
-        this.array = (T[]) new Object[index];
+    private int size = 0;
+    // по конструктору пока есть вопросы
+    public SimpleArray (int length) {
+        this.array = (T[]) new Object [length];
         //this.array=array;
+    }
+
+    public int getSize() {
+        return size = array.length;
     }
 
     /*public static void main(String[] args) {// на до понять что я печатаю
         SimpleArray sim = new SimpleArray();
         //sim.printObject();
-        sim.set();
+        sim.add();
         System.out.println();
     }*/
 
-    public void add(T model) {
+    public void add(T model, int index) {
+        if (index < array.length) throw new IllegalStateException("Mistake");
         array[index] = model;
         index++;
     }
@@ -32,18 +37,36 @@ public class SimpleArray<T> {
     }
 
     public void set(int index, T model) {
-        if (index < array.length) {
-            throw new IllegalStateException("Mistake");
-        }
-        set(index, model);
+        array[index] = model;
     }
 
-    public int remove(int index) {
-        for (int i = index; i < array.length; i++) {
+    public void remove(int index) {
+        /*for (int i = index; i < array.length; i++) {
             array[index] = array[index--];
+        }*/
+        if (index < 0 || index > array.length) {
+            throw new IndexOutOfBoundsException();
         }
-        return index;
+        Object element;
+        System.arraycopy(array, index + 1, array, index, size - 1);
     }
 
+    public Iterator<T> iterator() {
+        //class SimArIt {
+            int index = 0;
 
+            public boolean hasNext() {
+               return  array.length > size;
+            }
+
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                T data = array[index++];
+                return (Iterator<T>) data;
+            }
+        //}
+        return index++;
+    }
 }
