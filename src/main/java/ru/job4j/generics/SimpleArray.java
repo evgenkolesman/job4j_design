3ccca612e6ee4ca8d3c73efd57bd16e524b47141
glavs <glavstroi_e@mail.ru>
2021-02-06 11:44:38 +0300

    Init travis & jacoco

--- src/main/java/ru/job4j/generics/SimpleArray.java
+++ src/main/java/ru/job4j/generics/SimpleArray.java
@@ -1,76 +1,49 @@
-/*package ru.job4j.generics;
+package ru.job4j.generics;
 
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 // Каркас.
-
-public class SimpleArray<T> implements Iterable<T> {
+public class SimpleArray<T> {
 
     private T[] array;
     private T model;
     private int index;
 
-    public SimpleArray() {
-        this.index = index;
+    public SimpleArray(int index) {
+        this.array = (T[]) new Object[index];
         //this.array=array;
     }
 
-    public static void main(String[] args) {// на до понять что я печатаю
+    /*public static void main(String[] args) {// на до понять что я печатаю
         SimpleArray sim = new SimpleArray();
-        sim.printObject();
+        //sim.printObject();
+        sim.set();
         System.out.println();
-    }
-
-    public int getSize() {
-        return array.length;
-    }
+    }*/
 
     public void add(T model) {
-        if (index < array.length) {
-            throw new IllegalStateException("Mistake");
-        }
+        array[index] = model;
+        index++;
+    }
+
+    public T get(int index) {
+        return array[index];
     }
 
     public void set(int index, T model) {
         if (index < array.length) {
             throw new IllegalStateException("Mistake");
         }
+        set(index, model);
     }
 
     public int remove(int index) {
-        index--;
-        return remove(array[index]);
+        for (int i = index; i < array.length; i++) {
+            array[index] = array[index--];
+        }
+        return index;
     }
 
-    public T get(T[] array) {
-        if (index < array.length) {
-            throw new IllegalStateException("Mistake");
-        }
-        return array[index];
-    }
 
-    public void printObject(T[] array) {
-        for (Iterator<T> it = array.iterator(); it.hasNext(); ) {
-            Object next = it.next();
-            System.out.println("Текущий элемент" + next);
-        }
-
-        public Iterator<T> iterator () { //пока ошибка в Итераторе
-            public boolean hasNext () {
-                return false;
-            }
-
-            public Integer next () {
-                if (!hasNext()) {
-                    throw new NoSuchElementException();
-                }
-                return iterator.next();
-            }
-        }
-    }
-}*/
-    /*@Override
-    public Iterator<T> iterator() {
-        return null;
-    }*/
+}
