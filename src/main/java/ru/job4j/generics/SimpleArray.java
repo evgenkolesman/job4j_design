469d57e752441cd6c5efcbb578aa28efa5cba313
glavs <glavstroi_e@mail.ru>
2021-02-08 13:29:31 +0300

    Init travis & jacoco

--- src/main/java/ru/job4j/generics/SimpleArray.java
+++ src/main/java/ru/job4j/generics/SimpleArray.java
@@ -22,13 +22,6 @@ public class SimpleArray<T> implements Iterable<T> {
         return size;
     }
 
-    /*public static void main(String[] args) {// на до понять что я печатаю
-        SimpleArray sim = new SimpleArray();
-        //sim.printObject();
-        sim.add();
-        System.out.println();
-    }*/
-
     public void add(T model, int index) {
         // тут можно применить Objects.checkIndex(index, size);
         if (index < array.length) {
@@ -46,7 +39,7 @@ public class SimpleArray<T> implements Iterable<T> {
     @SuppressWarnings("checkstyle:AvoidNestedBlocks")
     public void set(int index, T model) {
         Objects.checkIndex(index, size);
-            array[index] = model;
+        array[index] = model;
     }
 
     public void remove(int index) {
@@ -57,38 +50,76 @@ public class SimpleArray<T> implements Iterable<T> {
             throw new IndexOutOfBoundsException();
         }*/
         Objects.checkIndex(index, size);
-        System.arraycopy(array, index + 1, array, index - 1, size - 1);
+        System.arraycopy(array, index, array, index - 1, size - 1);
     }
 
-    public boolean hasNext() {
+    @SuppressWarnings("checkstyle:EmptyLineSeparator")
+    public Iterator<T> iterator() {
+
+        class IteratorSA implements Iterator<T> {
+            int value = 0;
+
+            @Override
+            public boolean hasNext() {
+                return array.length > size;
+            }
+
+            @Override
+            public T next() {
+                if (hasNext()) {
+                    return array[value++];
+                } else {
+                    throw new NoSuchElementException();
+                }
+            }
+        }
+
+        return new IteratorSA();
+
+    }
+}
+    /*public static void main(String[] args) {// на до понять что я печатаю
+        SimpleArray sim = new SimpleArray();
+        //sim.printObject();
+        sim.add();
+        System.out.println();
+    }*/
+
+     /* public T iterator() {
+                if (hasNext()) {
+                    return array[value++];
+                } else {
+                    return throw new NoSuchElementException();
+                }
+            }*/
+        /*public boolean hasNext() {
         return array.length > size;
-    }
+    }*/
 
-    public T next() {
+    /*public T next() {
         int index = 0;
         if (!hasNext()) {
             throw new NoSuchElementException();
         }
         T data = array[index++];
-        return data;
-    }
+        return data;*/
 
     /*class IteratorSA<T> {
         public T iterator() {
             if (hasNext()) {
-                return array.next();
+                return (T) next();
             } else {
                 return null;
             }
         }
     }*/
-    @Override
-    public Iterator<T> iterator() {
+
+    /*public Iterator<T> iterator() {
         if (hasNext()) {
             return new Iterator<T>() {
                 @Override
                 public boolean hasNext() {
-                    return false;
+                    return SimpleArray.this.hasNext();
                 }
 
                 @Override
@@ -99,5 +130,5 @@ public class SimpleArray<T> implements Iterable<T> {
         } else {
             return null;
         }
-    }
-}
+    }*/
+
