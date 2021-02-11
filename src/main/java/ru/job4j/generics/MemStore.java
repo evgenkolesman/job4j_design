289d4758edd8f7caf50ea4df1d2b8c880d0e0f0f
glavs <glavstroi_e@mail.ru>
2021-02-11 18:09:25 +0300

    Init travis & jacoco

--- src/main/java/ru/job4j/generics/MemStore.java
+++ src/main/java/ru/job4j/generics/MemStore.java
@@ -29,6 +29,7 @@ public class MemStore<T extends Base> implements Store<T> {
     @Override
     public T findById(String id) {
         T data = null;
+        //for (T t : mem) если hasNext() не нужен
         for (Iterator<T> it = mem.iterator(); it.hasNext();) {
             data = it.next();
             //return null;
