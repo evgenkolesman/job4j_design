c2aa914c26f04f747ac9223d01e0d117cee4f2e8
glavs <glavstroi_e@mail.ru>
2021-02-12 09:43:42 +0300

    Init travis & jacoco

--- src/main/java/ru/job4j/generics/GenericsClass.java
+++ src/main/java/ru/job4j/generics/GenericsClass.java
@@ -2,22 +2,22 @@ package ru.job4j.generics;
 
 import java.security.Key;
 
-public class GenericsClass <K,V> {
+public class GenericsClass<K, V> {
     private K key;
 
     private V value;
 
-    public GenericsClass (K key, V value) {
-    this.key=key;
-    this.value=value;
+    public GenericsClass(K key, V value) {
+        this.key = key;
+        this.value = value;
     }
 
-    public String toString () {
-        return ("GenericClass" +"{Key= " + key
+    public String toString() {
+        return ("GenericClass" + "{Key= " + key
                 + ",Value= " + value + "}");
     }
 
-    public static void main (String[]arcs) {
+    public static void main(String[] arcs) {
         GenericsClass<String, String> gen = new GenericsClass<>("First key", "First value");
         System.out.println("Вывод в консоль: " + gen);
 
