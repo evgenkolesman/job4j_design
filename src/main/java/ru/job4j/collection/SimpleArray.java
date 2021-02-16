6dd49e395884ed2430a73644a76ed8084d0c1e60
glavs <glavstroi_e@mail.ru>
2021-02-16 23:29:33 +0300

    Init travis & jacoco

---
+++ src/main/java/ru/job4j/collection/SimpleArray.java
@@ -0,0 +1,87 @@
+package ru.job4j.collection;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+@SuppressWarnings("checkstyle:EmptyLineSeparator")
+public class SimpleArray<T> implements Iterable<T> {
+    private Object[] container = new Object[10];
+    public T[] containerNew = (T[]) container;
+    private int size;
+    public int index;
+    public int modCount;
+
+    public SimpleArray() {
+        this.containerNew = containerNew;
+        size = container.length;
+    }
+
+    public int getmodCount() {
+        // изменилось или длина, или значение одного из элемента массива
+        for (int i = 0; i < containerNew.length; i++) {
+            if (size != containerNew.length) {
+                modCount++;
+            } else if (!(containerNew[i] == container[i])) {
+                modCount++;
+            }
+        }
+        return modCount;
+    }
+
+    public T get(int index) {
+        if (containerNew[index] == null) {
+            throw new IndexOutOfBoundsException();
+        }
+        Objects.checkIndex(index, size);
+        return (T) containerNew[index];
+    }
+
+    public void add(T model) {
+        if (index < containerNew.length) {
+            containerNew[index] = model;
+        } else {
+            size++;
+            containerNew[size--] = model;
+        }
+
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        int expectedModCount = modCount;
+        class Iterator1 implements Iterator<T> {
+            int value = 0;
+            int count = 0;
+
+            public boolean hasNext() {
+                return size > value && containerNew[value] != null;
+            }
+
+            public T next() {
+
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                if (getmodCount() != expectedModCount) {
+                    throw new ConcurrentModificationException();
+                } else {
+                    //T data = containerNew[value++];
+                    return containerNew[value++];
+                }
+            }
+        }
+
+        return new Iterator1();
+
+    }
+}
+    /*public boolean hasNext () {
+        int i = 0;
+        while (size>i) {
+            i++;
+        }
+        return containerNew.length>i;
+    }*/
+
