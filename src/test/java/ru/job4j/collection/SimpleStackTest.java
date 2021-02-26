faa1bf229fc3a64e3ae34ee47b4b628ff4848419
glavs <glavstroi_e@mail.ru>
2021-02-26 13:33:21 +0300

    Init travis & jacoco

---
+++ src/test/java/ru/job4j/collection/SimpleStackTest.java
@@ -0,0 +1,38 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class SimpleStackTest {
+
+    @Test
+    public void whenPushThenPoll() {
+        SimpleStack <Integer> stack = new SimpleStack<>();
+        stack.push(1);
+        assertThat(stack.pop(), is(1));
+    }
+
+    @Test
+    public void whenPushPollThenPushPoll() {
+        SimpleStack <Integer> stack = new SimpleStack<>();
+        stack.push(1);
+        stack.pop();
+        stack.push(2);
+        assertThat(stack.pop(),is(2));
+    }
+
+    @Test
+    public void whenPushPushThenPollPoll() {
+        SimpleStack <Integer> stack = new SimpleStack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.pop();
+        assertThat(stack.pop(), is(1));
+    }
+
+
+
+
+}
