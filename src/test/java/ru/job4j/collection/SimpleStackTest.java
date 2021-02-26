b9908c9392b3b03a587e0f3164014470b2b6835e
glavs <glavstroi_e@mail.ru>
2021-02-26 13:41:12 +0300

    Init travis & jacoco

--- src/test/java/ru/job4j/collection/SimpleStackTest.java
+++ src/test/java/ru/job4j/collection/SimpleStackTest.java
@@ -9,30 +9,28 @@ public class SimpleStackTest {
 
     @Test
     public void whenPushThenPoll() {
-        SimpleStack <Integer> stack = new SimpleStack<>();
+        SimpleStack<Integer> stack = new SimpleStack<>();
         stack.push(1);
         assertThat(stack.pop(), is(1));
     }
 
     @Test
     public void whenPushPollThenPushPoll() {
-        SimpleStack <Integer> stack = new SimpleStack<>();
+        SimpleStack<Integer> stack = new SimpleStack<>();
         stack.push(1);
         stack.pop();
         stack.push(2);
-        assertThat(stack.pop(),is(2));
+        assertThat(stack.pop(), is(2));
     }
 
     @Test
     public void whenPushPushThenPollPoll() {
-        SimpleStack <Integer> stack = new SimpleStack<>();
+        SimpleStack<Integer> stack = new SimpleStack<>();
         stack.push(1);
         stack.push(2);
         stack.pop();
         assertThat(stack.pop(), is(1));
     }
-
-
 
 
 }
