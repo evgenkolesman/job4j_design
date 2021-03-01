c71eb41e3058abcac9ef2cd758502dcc8ea30cd0
glavs <glavstroi_e@mail.ru>
2021-03-01 10:47:47 +0300

    Init travis & jacoco

--- src/main/java/ru/job4j/collection/SimpleQueue.java
+++ src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -3,19 +3,34 @@ package ru.job4j.collection;
 //import java.util.NoSuchElementException;
 //import java.util.Objects;
 
+import java.util.NoSuchElementException;
+
 public class SimpleQueue<T> {
     private final SimpleStack<T> in = new SimpleStack<>();
     private final SimpleStack<T> out = new SimpleStack<>();
     private int index = 0;
+    private int index1 = 0;
+
 
     public T poll() {
+        if (index == 0) {
+            throw new NoSuchElementException();
+        }
+        if (index1 == 0) {
             while (index != 0) {
                 out.push(in.pop());
                 index--;
-                //index1++;
+                index1++;
             }
+        }
 
-        return out.pop();
+        T temp = null;
+        if (index1 != 0) {
+            temp = out.pop();
+            index1--;
+        }
+        //return out.pop();
+        return temp;
     }
 
     public void push(T value) {
