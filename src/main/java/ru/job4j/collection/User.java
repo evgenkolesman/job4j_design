4dcf39c63f874b36e26a997cfce3f69e5df6bc92
glavs <glavstroi_e@mail.ru>
2021-03-09 22:29:56 +0300

    Init travis & jacoco

---
+++ src/main/java/ru/job4j/collection/User.java
@@ -0,0 +1,15 @@
+package ru.job4j.collection;
+
+import java.util.Calendar;
+
+public class User {
+    private String name;
+    private int children;
+    private Calendar birthday;
+
+    public User(String name, int children, Calendar birthday) {
+        name = this.name;
+        children = this.children;
+        birthday = this.birthday;
+    }
+}
