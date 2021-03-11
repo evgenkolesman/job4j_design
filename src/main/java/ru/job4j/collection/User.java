b63db42e541c222dc73998fa529ebd4b585a3e50
glavs <glavstroi_e@mail.ru>
2021-03-11 23:56:28 +0300

    Init travis & jacoco

--- src/main/java/ru/job4j/collection/User.java
+++ src/main/java/ru/job4j/collection/User.java
@@ -27,5 +27,14 @@ public class User {
         map.put(second, element1);
         System.out.println("MAP = " + map);
         //System.out.println("User 1 = " + first + " User 2 = " + second);
+        Calendar calendar1 = new GregorianCalendar(2018, Calendar.NOVEMBER, 3);
+        Calendar calendar2 = new GregorianCalendar(2017, Calendar.OCTOBER, 10);
+        User third = new User("Irwin ", 1, calendar1);
+        User forth = new User("Lary ", 3, calendar1);
+        Object element3 = 3;
+        Object element4 = 4;
+        map.put(first, element3);
+        map.put(second, element4);
+        System.out.println("MAP = " + map);
     }
 }
