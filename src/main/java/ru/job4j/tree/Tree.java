83717aef4e40973a5f506ac818ec3f8e4f2bca3f
glavs <glavstroi_e@mail.ru>
2021-03-22 17:38:58 +0300

    Init travis & jacoco

--- src/main/java/ru/job4j/tree/Tree.java
+++ src/main/java/ru/job4j/tree/Tree.java
@@ -4,7 +4,6 @@ import java.util.*;
 
 class Tree<E> implements SimpleTree<E> {
     private final Node<E> root;
-
     public Tree(final E root) {
         this.root = new Node<>(root);
     }
@@ -12,6 +11,14 @@ class Tree<E> implements SimpleTree<E> {
     @Override
     public boolean add(E parent, E child) {
         boolean rsl = false;
+        findBy(parent);
+        if (Objects.equals(parent, child)||Objects.equals(root, child)) {
+        return rsl; }
+        else {
+            Node<E>  child1 = new Node<>(child);
+            root.children.add(child1);
+            rsl = true;
+        }
         return rsl;
     }
 
