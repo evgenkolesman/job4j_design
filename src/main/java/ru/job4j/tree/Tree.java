5e8c99200f61f8559785eea85cc05faad915a3bb
glavs <glavstroi_e@mail.ru>
2021-03-24 13:10:43 +0300

    Init travis & jacoco

--- src/main/java/ru/job4j/tree/Tree.java
+++ src/main/java/ru/job4j/tree/Tree.java
@@ -16,7 +16,7 @@ class Tree<E> implements SimpleTree<E> {
         Optional<Node<E>> parentNode = findBy(parent);
         Optional<Node<E>> childNode = findBy(child);
         if (parentNode.isPresent() && childNode.isEmpty()) {
-            parentNode.get().children.add(new Node<E>(child));
+            parentNode.get().children.add(new Node<>(child));
             rsl = true;
         }
 
@@ -26,36 +26,31 @@ class Tree<E> implements SimpleTree<E> {
 
     @Override
     public Optional<Node<E>> findBy(E value) {
-        Optional<Node<E>> rsl = Optional.empty();
-        Queue<Node<E>> data = new LinkedList<>();
-        data.offer(this.root);
-        while (!data.isEmpty()) {
-            Node<E> el = data.poll();
-            if (el.value.equals(value)) {
-                rsl = Optional.of(el);
-                break;
-            }
-            data.addAll(el.children);
-        }
-        return rsl;
+        return findByPredicate(x -> x.value.equals(value));
     }
 
     public boolean isBinary() {
-        boolean rsl = false;
-        E value;
-        if (findByPredicate()) {
-            rsl = true;
-        }
-        return rsl;
+        return findByPredicate(x -> x.children.size() > 2).isEmpty();
     }
 
-    private Optional<Node> findByPredicate(Predicate<Node<E>> condition) {
-        E value = new Node<>();
 
-        findBy()
-       if()
-    return
+    private Optional<Node<E>> findByPredicate(Predicate<Node<E>> condition) {
+        Optional<Node<E>> rsl = Optional.empty();
+        Queue<Node<E>> data = new LinkedList<>();
+        data.offer(this.root);
+            while (!data.isEmpty()) {
+                Node<E> el = data.poll();
+                if (condition.test(el)) {
+                    rsl = Optional.of(el);
+                    break;
+                }
+                data.addAll(el.children);
+            }
+
+            return rsl;
+
     }
+}
 
    /* public Iterator<Node<E>> iterator() {
         return new Iterator<Node<E>>() {
@@ -72,4 +67,4 @@ class Tree<E> implements SimpleTree<E> {
             }
         };
     }*/
-}
+
--- src/test/java/ru/job4j/tree/TreeTest.java
+++ src/test/java/ru/job4j/tree/TreeTest.java
@@ -30,4 +30,29 @@ public class TreeTest {
                 is(false)
         );
     }
+
+    @Test
+    public void isBinaryTestTrue() {
+        Tree<Integer> tree = new Tree<>(1);
+        tree.add(1, 2);
+        tree.add(1, 5);
+        tree.add(2,6);
+        assertThat(
+                tree.isBinary(),
+                is(true));
+    }
+
+    @Test
+    public void isBinaryTestFalse() {
+        Tree<Integer> tree = new Tree<>(1);
+        tree.add(1, 2);
+        tree.add(1, 4);
+        tree.add(1, 3);
+        tree.add(2,5);
+        assertThat(
+                tree.isBinary(),
+                is(false));
+    }
+
+
 }
