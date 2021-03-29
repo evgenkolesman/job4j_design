0bd4be1eed842d0e1dbcbcfa9b9423de3b225717
glavs <glavstroi_e@mail.ru>
2021-03-29 13:55:29 +0300

    Init travis & jacoco

--- src/main/java/ru/job4j/analize/Analize.java
+++ src/main/java/ru/job4j/analize/Analize.java
@@ -2,7 +2,8 @@ package ru.job4j.analize;
 
 import java.util.*;
 
-/*Сначала пошел через простое написание тестов под них стал выстраивать, через
+/*Сначала пошел через простое написание тестов, под них стал выстраивать метод diff и классы, методы извлекающие
+id и name считаю лишними, начал через
 простой цикл, итераторы, а потом решил применить Map ,
 так как на лицо классический вариант пар ключ - значение
  */
@@ -12,26 +13,26 @@ public class Analize {
     public Info diff(List<User> previous, List<User> current) {
         int deleted = 0;
         int changed = 0;
-        int added = 0;
+        int added;
 
         HashMap<Integer, String> allUsers = new HashMap<>();
 
         for (User index : current) {
-            allUsers.put(index.getId(), index.getName());
+            allUsers.put(index.id, index.name);
         }
 
         for (User index : previous) {
-            if (!allUsers.containsKey(index.getId())) {
+            if (!allUsers.containsKey(index.id)) {
                 deleted++;
-            } else if (!allUsers.get(index.getId()).equals(index.getName())) {
+            } else if (!allUsers.get(index.id).equals(index.name)) {
                 changed++;
             }
-            allUsers.put(index.getId(), index.getName());
+            allUsers.put(index.id, index.name);
         }
 
         int newSize = allUsers.size();
         added = newSize - previous.size();
-        return new Info(added,changed,deleted);
+        return new Info(added, changed, deleted);
     }
 
     public static class User {
@@ -41,14 +42,6 @@ public class Analize {
         public User(int id, String name) {
             this.id = id;
             this.name = name;
-        }
-
-        public int getId() {
-            return this.id;
-        }
-
-        public String getName() {
-            return this.name;
         }
     }
 
