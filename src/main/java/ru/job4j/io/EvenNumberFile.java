ed7027278addcda0f729be1db0dfd655705c68de
glavs <glavstroi_e@mail.ru>
2021-03-31 16:46:18 +0300

    Init travis & jacoco

--- src/main/java/ru/job4j/io/EvenNumberFile.java
+++ src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -2,24 +2,25 @@ package ru.job4j.io;
 
 import java.io.FileInputStream;
 
-public class ReadFile {
+public class EvenNumberFile {
     public static void main(String[] args) {
-        try (FileInputStream in = new FileInputStream("input.txt")) {
+        try (FileInputStream in = new FileInputStream("even.txt")) {
             StringBuilder text = new StringBuilder();
             int read;
             while ((read = in.read()) != -1) {
                 text.append((char) read);
             }
-            System.out.println(text);
-
             String[] lines = text.toString().split(System.lineSeparator());
             for (String line : lines) {
-                System.out.println(line);
+                int a = Integer.parseInt(line);
+            if (a % 2 == 0 ) {
+                    System.out.println(line); }
             }
-
-        } catch (Exception e) {
-            e.printStackTrace();
+        }
+        catch(Exception e){
+                e.printStackTrace();
+            }
         }
     }
-}
+
 
