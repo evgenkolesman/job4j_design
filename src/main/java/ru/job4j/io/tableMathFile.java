3c8fb9a2361c343da9edef2eaac7755c98f5f3d8
glavs <glavstroi_e@mail.ru>
2021-04-01 23:40:16 +0300

    Init travis & jacoco

--- src/main/java/ru/job4j/io/tableMathFile.java
+++ src/main/java/ru/job4j/io/tableMathFile.java
@@ -1,12 +1,28 @@
 package ru.job4j.io;
 
 import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class tableMathFile {
 
     public static void main(String[] args) {
-        StringBuilder tableMath = new StringBuilder(
-        "1*2=2 " + "2*2=4 " + "2*3=6 " + "2*4=8 " + "2*5=10 " + "2*6=12 " + "2*7=14 " + "2*8=16 " + "2*9=18");
+        int i = 9;
+        int z = 9;
+        StringBuilder tableMath = new StringBuilder();
+        String [][] arr = new String[i][z];
+
+        for (int a = 0; a < arr.length; a++) {
+            List<String> line =  new ArrayList<>();
+            for(int b = 0; b < arr[a].length; b++) {
+                arr[a][b] = " " + String.valueOf(a+1) + "*" + String.valueOf(b+1) +
+                        "=" + String.valueOf((a+1)*(b+1)) + " ";
+                line.add(arr[a][b]);
+                }
+            tableMath.append(line);
+        }
+
+        //tableMath.append(arr.toString());
         try (FileOutputStream out = new FileOutputStream("tableMath.txt")) {
             out.write(tableMath.toString().getBytes());
         } catch (Exception e) {
@@ -14,3 +30,5 @@ public class tableMathFile {
         }
     }
 }
+
+//"1*2=2 " + "2*2=4 " + "2*3=6 " + "2*4=8 " + "2*5=10 " + "2*6=12 " + "2*7=14 " + "2*8=16 " + "2*9=18"
