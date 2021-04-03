5297d0649a6e580bc0666809b6567fb11e001217
glavs <glavstroi_e@mail.ru>
2021-04-03 11:55:51 +0300

    Init travis & jacoco

--- src/main/java/ru/job4j/io/tableMathFile.java
+++ src/main/java/ru/job4j/io/tableMathFile.java
@@ -10,7 +10,7 @@ public class tableMathFile {
         int i = 9;
         int z = 9;
         StringBuilder tableMath = new StringBuilder();
-        new StringBuilder(System.lineSeparator());
+        StringBuilder a1 =new StringBuilder(System.lineSeparator());
         String[][] arr = new String[i][z];
 
         for (int a = 0; a < arr.length; a++) {
@@ -18,8 +18,11 @@ public class tableMathFile {
             for (int b = 0; b < arr[a].length; b++) {
                 arr[a][b] = String.format("%d * %d = %d ", a + 1, b + 1, (a + 1) * (b + 1));
                 line.add(arr[a][b]);
+
             }
             tableMath.append(line);
+            tableMath.append(a1);
+
         }
         try (FileOutputStream out = new FileOutputStream("tableMath.txt")) {
             out.write(tableMath.toString().getBytes());
