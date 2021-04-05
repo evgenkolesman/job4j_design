8d7828fb4c828720a60f6150d789d59e7c2f6dbf
glavs <glavstroi_e@mail.ru>
2021-04-05 23:40:46 +0300

    Init travis & jacoco

--- src/test/java/ru/job4j/io/ConfigTest.java
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -21,4 +21,23 @@ public class ConfigTest {
         config.load();
         assertThat(config.value("first comment "),is(" Evgen Koles"));
     }
+
+    @Test
+    public void whenAppPropertiesTest() {
+        String path = "app.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThatApp(config);
+    }
+
+    // проверка ctrl+alt+m - вынос одинаковых методов в один
+    private void assertThatApp(Config config) {
+        assertThat(config.value("hibernate.dialect"), is("org.hibernate.dialect.PostgreSQLDialect"));
+        assertThat(config.value("hibernate.connection.url"), is("jdbc:postgresql://127.0.0.1:5432/trackstudio"));
+        assertThat(config.value("hibernate.connection.driver_class"), is("org.postgresql.Driver"));
+        assertThat(config.value("hibernate.connection.username"), is("postgres"));
+        assertThat(config.value("hibernate.connection.password"), is("password"));
+    }
+
+
 }
