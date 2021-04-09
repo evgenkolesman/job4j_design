a355e056e636659db4d47a9d67d860b81594c0c4
glavs <glavstroi_e@mail.ru>
2021-04-09 17:49:35 +0300

    Init travis & jacoco

--- src/main/java/ru/job4j/io/Dir.java
+++ src/main/java/ru/job4j/io/Dir.java
@@ -1,6 +1,7 @@
 package ru.job4j.io;
 
 import java.io.File;
+import java.util.Objects;
 
 public class Dir {
     public static void main(String[] args) {
@@ -11,11 +12,11 @@ public class Dir {
         if (!file.isDirectory()) {
             throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
         }
-        System.out.println(String.format("size : %s", file.getTotalSpace()));
-        for (File subfile : file.listFiles()) {
+        System.out.printf("size : %s%n", file.getTotalSpace());
+        for (File subfile : Objects.requireNonNull(file.listFiles())) {
             String a = String.valueOf(subfile.length());
             String b = subfile.getAbsoluteFile().toString();
-            System.out.println(String.format("название : %s         размер : %s", b, a));
+            System.out.printf("название : %s  -----  размер : %s%n", b, a);
         }
     }
 }
