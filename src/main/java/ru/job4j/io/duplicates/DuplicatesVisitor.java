1ba6da9746db95d72ee45ec0c56ad2c3809d4e6a
glavs <glavstroi_e@mail.ru>
2021-04-17 13:04:10 +0300

    Init travis & jacoco

--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -5,11 +5,19 @@ import java.nio.file.FileVisitResult;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.HashSet;
+import java.util.Set;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
+    private final Set<FileProperty> fileProp = new HashSet<>();
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        System.out.println(file.toAbsolutePath());
+        FileProperty fileProperty = new FileProperty(file.toFile().length(), file.getFileName().toString());
+        if (fileProp.contains(fileProperty)) {
+            System.out.println(file.toAbsolutePath());
+        } else {
+            fileProp.add(fileProperty);
+        }
         return super.visitFile(file, attrs);
     }
 }
