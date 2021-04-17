4f0783f3270e4f372f51dcf5de4afa5b6dfa5f22
glavs <glavstroi_e@mail.ru>
2021-04-17 13:07:37 +0300

    Init travis & jacoco

--- src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
@@ -6,6 +6,6 @@ import java.nio.file.Path;
 
 public class DuplicatesFinder {
     public static void main(String[] args) throws IOException {
-        Files.walkFileTree(Path.of("./"), new DuplicatesVisitor());
+        Files.walkFileTree(Path.of("C:"), new DuplicatesVisitor());
     }
 }
