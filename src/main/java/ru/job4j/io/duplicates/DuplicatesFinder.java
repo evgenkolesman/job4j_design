cd71889afd2414ca0871df9aec115d130c2f721d
glavs <glavstroi_e@mail.ru>
2021-04-19 11:15:58 +0300

    Init travis & jacoco

--- src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
@@ -6,8 +6,11 @@ import java.nio.file.Path;
 
 public class DuplicatesFinder {
     public static void main(String[] args) throws IOException {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Root folder is null. Usage java -jar dir.jar ROOT_FOLDER.");
+        }
         for (String arg : args) {
-            Files.walkFileTree(Path.of(arg), new DuplicatesVisitor());
+            Files.walkFileTree(Path.of(args[0]), new DuplicatesVisitor());
         }
     }
 }
