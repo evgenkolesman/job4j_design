015b387c223a5eb43d7bc508d0ec4e9aca664f85
glavs <glavstroi_e@mail.ru>
2021-04-20 10:28:13 +0300

    Исправление в задаче 5. Валидация параметров запуска. [#449791] Добавлено: 1. Валидация по значению args 2. Исправлен источник пути и по расширению

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -9,12 +9,21 @@ import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
+/* поиск в файловой системе
+ *Поиск в файловой системе по заданным параметрам
+ *@author Kolesnikov Evgeniy (evgeniysanich@mail.ru)
+ * @version 1.0
+ */
+
 public class Search {
 
     public static void main(String[] args) {
-        Path start = Paths.get(".");
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Не верно задан аргумент : Root папка равна null. Надо так:  java -jar Search.jar ROOT_FOLDER");
+        }
+        Path start = Paths.get(args[0]);
         try {
-            List<Path> arr = new ArrayList<>(search(start, p -> p.toFile().getName().endsWith(".txt")));
+            List<Path> arr = new ArrayList<>(search(start, p -> p.toFile().getName().endsWith(args[1])));
             List<String> arr1 = arr.stream().map(ar -> ar.getFileName().toString()).collect(Collectors.toList());
             for (String i : arr1) {
                 System.out.println(i.substring(0, i.length() - 4) + " -- тип файла -> .txt");
