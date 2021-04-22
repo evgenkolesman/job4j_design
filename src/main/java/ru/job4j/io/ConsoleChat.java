3fec4cac0b5e52be8d342ffddeceb722024c1ec9
glavs <glavstroi_e@mail.ru>
2021-04-22 21:52:13 +0300

    6. Кодировка. [#449801] Запись ConsoleChat наметки метода run() 2

--- src/main/java/ru/job4j/io/ConsoleChat.java
+++ src/main/java/ru/job4j/io/ConsoleChat.java
@@ -1,13 +1,14 @@
 package ru.job4j.io;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.nio.CharBuffer;
 import java.util.Scanner;
 
 /* Создание консольного чата
  * Утилита для создания консольного чата, который
- * останавливается при команде стоп,  продолжает при продолжить,заканчивает при команде Закончить.
+ * останавливается при команде стоп,
+ * продолжает при продолжить,
+ * заканчивает при команде закончить.
  *@author Kolesnikov Evgeniy (evgeniysanich@mail.ru)
  * @version 1.0
  */
@@ -17,8 +18,8 @@ public class ConsoleChat {
     private static final String OUT = "закончить";
     private static final String STOP = "стоп";
     private static final String CONTINUE = "продолжить";
-    public String target = "targetText.txt";
-    public String source = "sourceText.txt";
+    public static String target = "targetText.txt";
+    public static String source = "sourceText.txt";
 
     public ConsoleChat(String path, String botAnswers) {
         this.path = path;
@@ -29,26 +30,35 @@ public class ConsoleChat {
         Scanner console = new Scanner(System.in);
         String data = console.nextLine();
         while (!data.isEmpty()) {
-            try(BufferedWriter wr = new BufferedWriter(new FileWriter(target))) {
+            try (BufferedWriter wr = new BufferedWriter(new FileWriter(target))) {
                 wr.append(data);
+                if (data.equals(OUT)) {
+                    System.exit(0);
+                }
+
+                if (data.equals(STOP)) {
+                    try {
+                        wait();
+                    } catch (InterruptedException e) {
+                    }// не правильно надо придумать стоп
+                }
+                if (data.equals(CONTINUE)) {
+                    notify();
+                }
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            if (data.equals(OUT)) {
-                break;
-            }
 
-            if (data.equals(STOP)) {
+            try (BufferedReader out = new BufferedReader(new FileReader(source))) {
+                System.out.println(out.readLine());
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-
-            if (data.equals(CONTINUE)) {
-            }
-
         }
     }
 
-    public void main(String[] args) {
-        ConsoleChat cc = new ConsoleChat("","");
+    public static void main(String[] args) {
+        ConsoleChat cc = new ConsoleChat(target, source);
         cc.run();
     }
 }
