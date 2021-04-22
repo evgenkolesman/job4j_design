66975e8f76688db3cf1118cb7a7e2e426ee96f7c
glavs <glavstroi_e@mail.ru>
2021-04-22 16:17:42 +0300

    6. Кодировка. [#449801] Запись ConsoleChat наметки метода run()

--- src/main/java/ru/job4j/io/ConsoleChat.java
+++ src/main/java/ru/job4j/io/ConsoleChat.java
@@ -1,5 +1,10 @@
 package ru.job4j.io;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 /* Создание консольного чата
  * Утилита для создания консольного чата, который
  * останавливается при команде стоп,  продолжает при продолжить,заканчивает при команде Закончить.
@@ -12,13 +17,35 @@ public class ConsoleChat {
     private static final String OUT = "закончить";
     private static final String STOP = "стоп";
     private static final String CONTINUE = "продолжить";
+    public String target = "targetText.txt";
+    public String source = "sourceText.txt";
 
     public ConsoleChat(String path, String botAnswers) {
         this.path = path;
         this.botAnswers = botAnswers;
     }
 
-    public void run() {}
+    public void run() {
+        Scanner console = new Scanner(System.in);
+        String data = console.nextLine();
+        while (!data.isEmpty()) {
+            try(BufferedWriter wr = new BufferedWriter(new FileWriter(target))) {
+                wr.append(data);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (data.equals(OUT)) {
+                break;
+            }
+
+            if (data.equals(STOP)) {
+            }
+
+            if (data.equals(CONTINUE)) {
+            }
+
+        }
+    }
 
     public void main(String[] args) {
         ConsoleChat cc = new ConsoleChat("","");
