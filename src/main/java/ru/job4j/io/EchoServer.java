5426acfcb62f0cf14764012b1f798288c826d7e4
glavs <glavstroi_e@mail.ru>
2021-04-27 14:31:13 +0300

    1. Бот [#449815] основы

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -4,11 +4,6 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-/* Знакомство с Socket
- * Выход из ситсемы при определенному запросу
- *@author Kolesnikov Evgeniy (evgeniysanich@mail.ru)
- * @version 1.0
- */
 public class EchoServer {
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
@@ -17,16 +12,22 @@ public class EchoServer {
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
-                    String str = in.readLine();
-                    while (!str.isEmpty()) {
-
+                    String str;
+                    while (!(str = in.readLine()).isEmpty()) {
                         System.out.println(str);
-                        if (str.contains("Bye")) {
+                        /*if (str.contains("Hello")) {
+                            out.write("Hello".getBytes());
+                        }
+                        if (str.contains("Exit")) {
                             System.exit(0);
                         }
-                        str = in.readLine();
+                        if (str.contains("What")) {
+                            out.write("What".getBytes());
+                        }*/
+                        //str = in.readLine();
                     }
                     out.write("HTTP/1.1 200 OK\r\n".getBytes());
+                    out.write("Hello, dear friend.".getBytes());
                 }
             }
         }
