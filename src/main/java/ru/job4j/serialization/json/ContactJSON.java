ffdad4d77836c335b5179b475f6fa5eb611f17e1
glavs <glavstroi_e@mail.ru>
2021-05-11 19:48:19 +0300

    Сделал 4. JAXB. Преобразование XML в POJO. [#449807] Добавил десериализацию, сначала просто не обратил внимание добавил коменты

---
+++ src/main/java/ru/job4j/serialization/json/ContactJSON.java
@@ -0,0 +1,19 @@
+package ru.job4j.serialization.json;
+
+/* Класс Contact
+ * собирает и выводит определенный формат данных:  номер телефона
+ *@author Kolesnikov Evgeniy (evgeniysanich@mail.ru)
+ * @version 1.0
+ */
+public class ContactJSON {
+    private final String phone;
+
+    public Contact(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Contact{phone='%s'}", phone);
+    }
+}
