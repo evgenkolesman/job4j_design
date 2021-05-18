3408347c21eb70e3cb8938aa66a0024a0fdd2421
glavs <glavstroi_e@mail.ru>
2021-05-19 00:11:52 +0300

    Подготовил решение. Поиск файлов по критерию [#449811] полностью готово основной класс Find, вспомогательный SearchFiles

--- src/main/java/ru/job4j/io/Searchfiles/Find.java
+++ src/main/java/ru/job4j/io/Searchfiles/Find.java
@@ -1,7 +1,6 @@
 package ru.job4j.io.searchfiles;
 
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -9,32 +8,48 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
+
+/*
+ * Программа ищет данные в заданном каталоге и подкаталогах.
+ * Имя файла может задаваться, целиком, по маске, по регулярному выражению(не обязательно).
+ * Программа должна собираться в jar и запускаться через java -jar find.jar -d=c:/ -n=*.txt -t=mask -o=log.txt
+ * Ключи
+ * -d - директория, в которой начинать поиск.
+ * -n - имя файла, маска, либо регулярное выражение.
+ * -t - тип поиска: mask искать по маске, name по полному совпадение имени, regex по регулярному выражению.
+ * -o - результат записать в файл.
+ * Программа записывает результат в файл log.txt.
+ * В программе должна быть валидация ключей и подсказка.
+ *
+ * @author Kolesnikov Evgeniy (evgeniysanich@mail.ru)
+ * @version 1.0
+ */
 
 public class Find {
-
-    private static String fileWrite = "log.txt";
+    private static final String fileWrite = "log1.txt";
 
     public static void main(String[] args) {
-        SearchFileVisitor searchFileVisitor = new SearchFileVisitor();
-        try {
-            Files.walkFileTree(Paths.get("c:\""), searchFileVisitor);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        searchFileVisitor.setPartOfName(".txt");
-
-        List<Path> foundfiles = searchFileVisitor.getFoundfiles();
-        for(Path a : foundfiles) {
-            writeInFile(fileWrite, a);
-        }
-    }
-
-    private static void writeInFile(String fileWrite, Path a) {
+        Path start = Paths.get("C:/");
+        List<Path> pathList = new ArrayList<>(search(start, p -> p.toFile().getName().endsWith("txt")));
         try(BufferedWriter br = new BufferedWriter(new FileWriter(fileWrite))) {
-            br.write(String.valueOf(a));
+            for (Path a : pathList) {
+            br.write(a.toString());
+            br.write(System.lineSeparator());
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+    public static List<Path> search(Path root, Predicate<Path> condition) {
+        SearchFiles searcher = new SearchFiles(condition);
+        try {
+            Files.walkFileTree(root, searcher);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return searcher.getPaths();
+    }
+
 }
