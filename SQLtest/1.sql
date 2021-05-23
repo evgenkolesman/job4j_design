c29d4b259f3fec241d8113bcc72498a1f4a2cb20
evgenkolesman <70563563+evgenkolesman@users.noreply.github.com>
2021-05-23 22:20:06 +0300

    Add files via upload

--- SQLtest/1.sql
+++ SQLtest/1.sql
@@ -7,4 +7,8 @@ create table cars(
 insert into cars(name, country, number) values('Gealy', 'China', '030');
 insert into cars(name, country, number) values('Jeep', 'USA', '020');
 insert into cars(name, country, number) values('Vesta', 'Russia', '010');
+update cars
+	set name = 'Cherry'
+	WHERE id = 1;
+select*from cars;
 delete from cars;
