fbeb56c97a78eda419710cdd7b1b3564ca6fdf83
evgenkolesman <70563563+evgenkolesman@users.noreply.github.com>
2021-05-29 08:36:27 +0300

    Add files via upload

--- SQLtest/insert.sql
+++ SQLtest/insert.sql
@@ -1,5 +1,5 @@
-insert into role(id,name) values (1, 'builder');
-insert into users(name, id_role) values ('Alex', 1);
+insert into role(id, name) values (1, 'builder');
+insert into users(id, name, id_role) values (1, 'Alex', 1);
 insert into state(id, name) values (10, 'Germany');
 insert into rules(id, name) values (100, 'norm');
 insert into category(id, name) values (7,'class');
@@ -7,3 +7,4 @@ insert into role_rules(name, id_role, id_rules) values('1', 1, 100);
 insert into item(id, name, id_state, id_category) values (1000, 'shovel', 10,7);
 insert into comments(name, id_item) values ('allright',1000);
 insert into attaches(name, id_item) values('Shovel 1', 1000);
+insert into users_item(id, name, users_id) values (1, 'First', 1);
