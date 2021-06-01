ffd2a4b80baedae43e641ac578ba28f8a6c251d4
evgenkolesman <70563563+evgenkolesman@users.noreply.github.com>
2021-06-01 15:52:26 +0300

    Add files via upload

---
+++ SQLtest/фильтр/createTP.SQL.sql
@@ -0,0 +1,17 @@
+insert into type(name) values ('Молоко');
+insert into type(name) values ('Сыр');
+insert into type(name) values ('Мороженное');
+
+insert into product(name, type_id, expired_date, price) values ('Молоко Буренка', 1, 2020-01-01, 100);
+insert into product(name, type_id, expired_date, price) values ('Молоко Варенка', 1, 2021-03-05, 120);
+insert into product(name, type_id, expired_date, price) values ('Молоко обезжиренное', 1, 2021-05-04, 115);
+insert into product(name, type_id, expired_date, price) values ('Молоко Парное', 1, 2021-05-26, 125);
+
+insert into product(name, type_id, expired_date, price) values ('Сыр Еланский', 2, 2021-05-20, 150);
+insert into product(name, type_id, expired_date, price) values ('Сыр Волжский', 2, 2021-05-21-5, 155);
+insert into product(name, type_id, expired_date, price) values ('Сыр Луговой', 2, 2021-05-22, 140);
+
+insert into product(name, type_id, expired_date, price) values ('Мороженное Рожок', 3, 2019-01-01, 55);
+insert into product(name, type_id, expired_date, price) values ('Мороженное Стаканчик', 3, 2020-01-10, 55);
+insert into product(name, type_id, expired_date, price) values ('Мороженное Эскимо', 3, 2020-10-25, 80);
+insert into product(name, type_id, expired_date, price) values ('Мороженное шоколадное', 3, 2021-03-26, 65);
