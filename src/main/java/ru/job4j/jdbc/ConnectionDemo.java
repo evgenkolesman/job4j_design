0d91bd113a10cc1493acd1d1a9271c9a7ed57d10
glavs <glavstroi_e@mail.ru>
2021-06-04 15:51:04 +0300

    Подготовил решение. Задание 0. JDBC [#6863] почему то етсь ошибка  свзаимодействием с БД

--- src/main/java/ru/job4j/jdbc/ConnectionDemo.java
+++ src/main/java/ru/job4j/jdbc/ConnectionDemo.java
@@ -18,7 +18,7 @@ import java.util.List;
  * и по ним  заходим  базу данных idea_db
  */
 public class ConnectionDemo {
-    private static File file = new File("C:\\projects\\job4j_design\\app.properties");
+    private static final File file = new File("C:\\projects\\job4j_design\\app.properties");
     private static String url;
     private static String login;
     private static String password;
