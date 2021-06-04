f48e4811874691ae0e156c0546a752e905dd947a
glavs <glavstroi_e@mail.ru>
2021-06-04 11:58:15 +0300

    Подготовил решение. Поиск файлов по критерию [#449811] полностью готово основной класс Find, вспомогательный SearchFiles Переписываю с коментариями. Цель понимание процесса поиска на осознанном уровне. Добавил условия поиска по маске и по имени проверку, с предикатом
    # Conflicts:
    #	SQLtest/фильтр/createTP.SQL.sql

--- SQLtest/фильтр/SelectTP.sql
+++ SQLtest/фильтр/SelectTP.sql
@@ -1,7 +1,8 @@
 select product.name from product join type on   type_id=type.id where type.name = 'Сыр';
 select name from product where name like '%Мороженное%';
 select name from product where  expired_date < current_date;
-select name from product group by name order by max(price);
+--select name from product group by name order by max(price);
+select * from products as p where p.price = (select max(price) from products);
 select name from product where (select max(price) price from product) > price limit(1);
 select type.name, count(type_id) from type join product on  type_id=type.id group by type.name;
 select product.name from product join type on   type_id=type.id 
