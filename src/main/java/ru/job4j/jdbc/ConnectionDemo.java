1541a71b6824dfaa191e5303e8644009a644d9c1
glavs <glavstroi_e@mail.ru>
2021-06-04 14:33:31 +0300

    Подготовил решение. Поиск файлов по критерию [#449811] полностью готово основной класс Find, вспомогательный SearchFiles Переписываю с коментариями. Цель понимание процесса поиска на осознанном уровне. Добавил условия поиска по маске и по имени проверку, с предикатом
    # Conflicts:
    #	SQLtest/фильтр/createTP.SQL.sql

--- src/main/java/ru/job4j/jdbc/ConnectionDemo.java
+++ src/main/java/ru/job4j/jdbc/ConnectionDemo.java
@@ -1,20 +1,65 @@
 package ru.job4j.jdbc;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ConnectionDemo {
-      public static void main(String[] args) throws ClassNotFoundException, SQLException {
+    private static File file = new File("C:\\projects\\job4j_design\\app.properties");
+    private static String url;
+    private static String login;
+    private static String password;
+    private static List<String> listadd = new ArrayList<>();
+
+    //String url, login, password;
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
         Class.forName("org.postgresql.Driver");
-        String url = "jdbc:postgresql://localhost:5432/idea_db";
+        readLine();
+        getData();
+        System.out.printf("%s%n%s%n%s%n", password, url, login);
+        /*String url = "jdbc:postgresql://localhost:5432/idea_db";
         String login = "postgres";
-        String password = "PassworD1";
-        try(Connection connection = DriverManager.getConnection(url, login, password)){
+        String password = "PassworD1";*/
+        try (Connection connection = DriverManager.getConnection(url, login, password)) {
             DatabaseMetaData metaData = connection.getMetaData();
             System.out.println(metaData.getUserName());
             System.out.println(metaData.getURL());
+        }
+    }
+
+    private static void getData() {
+        for (String ar : listadd) {
+            if (ar.contains("url")) {
+                String[] url1 = ar.split("=");
+                url = url1[1];
+            }
+            if (ar.contains("password")) {
+                String[] password1 = ar.split("=");
+                password = password1[1];
+            }
+            if (ar.contains("username") && (!ar.startsWith("#"))) {
+                String[] login1 = ar.split("=");
+                login = login1[1];
+            }
+        }
+    }
+
+    private static void readLine() {
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String a;
+            while ((a = br.readLine()) != null) {
+                listadd.add(a);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
