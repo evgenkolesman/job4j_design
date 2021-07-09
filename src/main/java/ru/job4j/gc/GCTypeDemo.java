cad6f19f59ef141082129d5adba87fcb4a09fc3c
evgenkolesman <glavstroi_e@mail.ru>
2021-07-10 02:39:40 +0300

    Firstly in linux

--- src/main/java/ru/job4j/gc/GCTypeDemo.java
+++ src/main/java/ru/job4j/gc/GCTypeDemo.java
@@ -7,7 +7,7 @@ public class GCTypeDemo {
         Random random = new Random();
         int length = 100;
         String[] data = new String[1_000_000];
-        for (int i = 0; ; i = (i + 1) % data.length) {
+        for (int i = 0;; i = (i + 1) % data.length) {
             data[i] = String.valueOf(
                     (char) random.nextInt(255)
             ).repeat(length);
