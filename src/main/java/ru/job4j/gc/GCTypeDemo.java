bc2c8f9bc479378e89d1fae33f79fd0e8990ee6d
glavs <glavstroi_e@mail.ru>
2021-07-14 11:51:00 +0300

    1. Демонстрация работы GC [#1589]

--- src/main/java/ru/job4j/gc/GCTypeDemo.java
+++ src/main/java/ru/job4j/gc/GCTypeDemo.java
@@ -2,12 +2,17 @@ package ru.job4j.gc;
 
 import java.util.Random;
 
+/*
+ * Демонстрация garbage collector
+ * @author Kolesnikov Evgeniy (evgeniysanich@mail.ru)
+ * @version 1.0
+ */
 public class GCTypeDemo {
     public static void main(String[] args) {
         Random random = new Random();
         int length = 100;
         String[] data = new String[1_000_000];
-        for (int i = 0; ; i = (i + 1) % data.length) {
+        for (int i = 0;; i = (i + 1) % data.length) {
             data[i] = String.valueOf(
                     (char) random.nextInt(255)
             ).repeat(length);
---
+++ src/main/java/ru/job4j/gc/ref/StrongDemo.java
@@ -0,0 +1,65 @@
+package ru.job4j.gc.ref;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/* Пример сильных ссылок
+ * example 3 с ошибкой
+ * настраиваем VM строку:  -Xmx4m -Xms4m
+ * @author Kolesnikov Evgeniy (evgeniysanich@mail.ru)
+ * @version 1.0
+ */
+
+public class StrongDemo {
+    public static void main(String[] args) throws InterruptedException {
+        example1();
+        example2();
+        example3();
+    }
+
+    private static void example1() throws InterruptedException {
+        Object[] objects = new Object[100];
+        for (int i = 0; i < 100; i++) {
+            objects[i] = new Object() {
+                @Override
+                protected void finalize() throws Throwable {
+                    System.out.println("Object removed!");
+                }
+            };
+        }
+        for (int i = 0; i < 100; i++) {
+            objects[i] = null;
+        }
+        System.gc();
+        TimeUnit.SECONDS.sleep(5);
+    }
+
+    private static void example2() throws InterruptedException {
+        Object[] objects = new Object[100];
+        for (int i = 0; i < 100; i++) {
+            Object object = new Object() {
+                Object innerObject = new Object() {
+                    @Override
+                    protected void finalize() throws Throwable {
+                        System.out.println("Remove inner object!");
+                    }
+                };
+            };
+            objects[i] = object;
+        }
+        for (int i = 0; i < 100; i++) {
+            objects[i] = null;
+        }
+        System.gc();
+        TimeUnit.SECONDS.sleep(5);
+    }
+
+    private static void example3() {
+        List<String> strings = new ArrayList<>();
+        while (true) {
+            strings.add(String.valueOf(System.currentTimeMillis()));
+        }
+    }
+}
+
