990757e6dac1074f3280c7389ee77420bbb3c822
glavs <glavstroi_e@mail.ru>
2021-07-15 16:50:27 +0300

    Пробное задание для банка ФФ

--- src/main/java/ru/job4j/testexersize/TestExersize.java
+++ src/main/java/ru/job4j/testexersize/TestExersize.java
@@ -7,7 +7,7 @@ public class TestExersize {
     public static void filter(Collection<BigDecimal> numbers) throws MyException {
         BigDecimal [] arr = numbers.toArray(new BigDecimal[numbers.size()]);
         for(BigDecimal n : numbers) {
-            if(n.compareTo(arr[arr.length-1].divide(arr[0])) >= 0) {
+            if(n.compareTo(arr[arr.length-1].divide(arr[0])) <= 0) {
                 numbers.remove(n);
             }
         }
