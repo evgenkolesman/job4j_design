9702fa6dd1082009f704a601e2334d99b0c44d62
glavs <glavstroi_e@mail.ru>
2021-07-23 10:22:32 +0300

    Устранение замечаний

--- src/main/java/ru/job4j/cache/AbstractCache.java
+++ src/main/java/ru/job4j/cache/AbstractCache.java
@@ -14,20 +14,16 @@ public abstract class AbstractCache<K, V> {
     protected final Map<K, SoftReference<V>> cache = new HashMap<>();
 
     public void put(K key, V value) {
-
         SoftReference<V> valueMap = new SoftReference(value);
         cache.put(key, valueMap);
     }
 
     public V get(K key) {
-
         V value = cache.getOrDefault(key, new SoftReference<V>(null)).get();
-
         if (value == null) {
             value = load(key);
             put(key, value);
         }
-
         return value;
     }
 
--- src/main/java/ru/job4j/cache/Emulator.java
+++ src/main/java/ru/job4j/cache/Emulator.java
@@ -16,29 +16,34 @@ import java.util.Scanner;
 public class Emulator {
 
     public static void main(String[] args) throws IOException {
-        System.out.println("Hello! Do you want to write cache from file? Y/N, else program will close");
+        System.out.println("Hello! Do you want to write cache from file? Y/N");
         var sc = new Scanner(System.in);
-        while (sc.next().equalsIgnoreCase("Y")) {
-            System.out.println("Write cache directory: ");
-            var resDirIn = sc.next();
+        var ans = sc.next();
+        System.out.println("Write cache directory: ");
+        var resDirIn = sc.next();
+        while (ans.equalsIgnoreCase("Y")) {
             System.out.println("Write file for input: ");
             var resDirIn1 = sc.next();
             DirFileCache dirFileCache = new DirFileCache(resDirIn);
             System.out.println("Write path for output: ");
             var resDirOut = sc.next();
-            if (!resDirIn1.isEmpty() || !dirFileCache.cache.isEmpty()) {
+            if (!dirFileCache.cache.isEmpty() || !resDirIn.isEmpty()) {
                 dirFileCache.writerFile(resDirIn1, resDirOut);
             } else {
-                System.out.println("Wrong file or directory!");
+                System.out.println("Wrong file!");
             }
-            System.out.println("If you want to get in console cache write 'Y', else write 'N'");
+            System.out.println("If you want to get in console and write file, print 'Y', else ptint 'N'");
             var answer = sc.next().toUpperCase();
             if (answer.equals("Y")) {
                 System.out.println(Files.readString(Path.of(resDirIn, resDirIn1), Charset.forName("WINDOWS-1251")));
-                System.out.println("Do you want to continue and write cache from file? Y/N");
-
+                System.out.println("Do you want to continue and write file? Y/N");
+                if (sc.next().equalsIgnoreCase("y")) {
+                    continue;
+                } else {
+                    break;
+                }
             } else {
-                System.out.println("Hello! Do you want to write cache from file? Y/N");
+                System.out.println("Hello! Do you want to write write file? Y/N");
             }
         }
     }
