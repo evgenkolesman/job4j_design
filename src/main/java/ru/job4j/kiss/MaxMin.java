69ec44fcdbce0b15828eb9f922cd973c76d0470d
glavs <glavstroi_e@mail.ru>
2021-07-27 12:27:30 +0300

    Исправление по Kiss

--- src/main/java/ru/job4j/kiss/MaxMin.java
+++ src/main/java/ru/job4j/kiss/MaxMin.java
@@ -2,22 +2,26 @@ package ru.job4j.kiss;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class MaxMin {
     public <T> T max(List<T> value, Comparator<T> comparator) {
         T v = value.get(0);
-        for (int i = 1; i < value.size() - 1; i++) {
-            if (comparator.compare(v, value.get(i + 1)) < 0) {
-                v = value.get(i + 1);
-            }
-        }
+        Predicate<Integer> predicate = a -> a < 0;
+        v = getT(value, comparator, v, predicate);
         return v;
     }
 
     public <T> T min(List<T> value, Comparator<T> comparator) {
         T v = value.get(0);
+        Predicate<Integer> predicate = a -> a > 0;
+        v = getT(value, comparator, v, predicate);
+        return v;
+    }
+
+    private <T> T getT(List<T> value, Comparator<T> comparator, T v, Predicate<Integer> predicate) {
         for (int i = 0; i < value.size() - 1; i++) {
-            if (comparator.compare(v, value.get(i + 1)) > 0) {
+            if (predicate.test(comparator.compare(v, value.get(i + 1)))) {
                 v = value.get(i + 1);
             }
         }
--- src/test/java/ru/job4j/kiss/MaxMinTest.java
+++ src/test/java/ru/job4j/kiss/MaxMinTest.java
@@ -9,7 +9,6 @@ import java.util.List;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
-
 public class MaxMinTest {
 
     @Test
