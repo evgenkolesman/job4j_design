8c2ece918cb2702c4b4789b4b3f44ce8217a3dec
glavs <glavstroi_e@mail.ru>
2021-07-27 13:01:58 +0300

    3. Шаблонизатор. [#855]

---
+++ src/test/java/ru/job4j/template/GeneratorTest.java
@@ -0,0 +1,44 @@
+package ru.job4j.template;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.util.Map;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
+
+public class GeneratorTest {
+
+    @Test
+    @Ignore
+    public void produce() {
+        Generator gen = new GeneratorV();
+        gen.produce(new String(), Map.of(new String(), new String()));
+        assertEquals(GeneratorV.class, gen.getClass());
+    }
+
+    @Test(expected = NullPointerException.class)
+    @Ignore
+    public void whenNulls() {
+        Generator gen = new GeneratorV();
+        gen.produce(null, Map.of(null, null));
+    }
+
+    @Test//(expected = Exception.class)
+    @Ignore
+    public void whenNullTemplate() {
+        Generator gen = new GeneratorV();
+        gen.produce(null, Map.of(new String(), new String()));
+        assertEquals(GeneratorV.class, gen.getClass());
+    }
+
+    @Test(expected = NullPointerException.class)
+    @Ignore
+    public void whenWrongMap() {
+        Generator gen = new GeneratorV();
+        gen.produce(new String(), Map.of(null, new String()));
+        assertThat(gen, is(new GeneratorV()));
+    }
+}
