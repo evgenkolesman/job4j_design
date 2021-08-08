c08f4b5df90ab9171425ae226d7c2e436d7ba077
glavs <glavstroi_e@mail.ru>
2021-08-08 09:44:33 +0300

    Изменил сделал сначала извлечение потом распределение, убрал часть полей просто в переменные в countFreshness, чем облегчил конструктор

--- src/main/java/ru/job4j/solid/lcp/foodstore/ControllQuality.java
+++ src/main/java/ru/job4j/solid/lcp/foodstore/ControllQuality.java
@@ -6,16 +6,16 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-/*
+/**
  * Этот метод делает ярлычки свежести для нашей системы хранения исходя,
  * каждый ярлычок мы вносим в продукт путем setFreshness()
  * на основании этого ярлычка будет произведена сортировка
- * @Kolesnikov Evgeniy evgeniysanich@mail.ru
+ * добавлен resort() производит пересортировку товаров
+ *
  * @version 1
+ * @Kolesnikov Evgeniy evgeniysanich@mail.ru
  */
 public class ControllQuality {
-    private int storageLife; // количество в днях годности продукта
-    private int nowDateProductLife; // осталось времени в днях годности
     private Date date = new Date();
     private List<Store> storesList;
 
@@ -24,8 +24,10 @@ public class ControllQuality {
     }
 
     public void countFreshness(Food food) {
-        storageLife = (int) ((food.getExpirityDate().getTimeInMillis() - food.getCreateDate().getTimeInMillis()) / (24 * 60 * 60 * 1000));
-        nowDateProductLife = (int) (date.getTime()
+        // количество в днях годности продукта
+        int storageLife = (int) ((food.getExpirityDate().getTimeInMillis() - food.getCreateDate().getTimeInMillis()) / (24 * 60 * 60 * 1000));
+        // осталось времени в днях годности
+        int nowDateProductLife = (int) (date.getTime()
                 - food.getCreateDate().getTimeInMillis()) / (24 * 60 * 60 * 1000);
         if (date.after(food.getExpirityDate().getTime())) {
             food.setFreshness(100);
@@ -43,8 +45,8 @@ public class ControllQuality {
 
     public void resort() {
         List<Food> foodList = new ArrayList<>();
-        for(Store s : storesList) {
-            s.getAll().forEach(foodList :: add);
+        for (Store s : storesList) {
+            foodList.addAll(s.getAll());
             s.clear();
         }
         foodList.forEach(this::distribute);
